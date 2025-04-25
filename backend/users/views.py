7ef2bf43669e7928db6d0d7db51ae6a2703cf7c1
AlHIO <alalthegamegun.c@nycu.edu.tw>
2025-04-25 22:48:34 +0800

    adopt views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+# users/views.py
+from rest_framework import generics
+from .serializers import RegisterSerializer, LoginSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
+
 # Create your views here.
